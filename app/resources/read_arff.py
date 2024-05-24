1f235fe7f97035d121a3a1dedd2de38803032cdc
Romulo Pauliv <romulopauliv@bk.ru>
2024-05-24 12:49:21 -0300

    added basic framework

---
+++ app/resources/read_arff.py
@@ -0,0 +1,59 @@
+# |--------------------------------------------------------------------------------------------------------------------|
+# |                                                                                analysis/app/resources/read_arff.py |
+# |                                                                                                    encoding: UTF-8 |
+# |                                                                                                     Python v: 3.10 |
+# |--------------------------------------------------------------------------------------------------------------------|
+
+# | Imports |----------------------------------------------------------------------------------------------------------|
+from scipy.io   import arff
+from pathlib    import PosixPath, Path
+
+import pandas   as pd
+
+from resources.bin_manager  import BinManager
+from config.config_files    import configfiles
+from log.genlog             import genlog
+# |--------------------------------------------------------------------------------------------------------------------|
+
+class ReadARFF(object):
+    """
+    A class to read ARFF files and cache the data in binary format for quicker access.
+
+    This class reads ARFF files from a specified directory, converts them to
+    pandas DataFrames, and uses a BinManager instance to cache the data for
+    future access.
+    """
+    def __init__(self) -> None:
+        """
+        Initialize the ReadARFF instance.
+
+        This sets the root path for ARFF files and the file extension,
+        and initializes a BinManager instance for managing binary file caching.
+        """
+        self.root_path  : PosixPath = Path(configfiles.dot_ini['paths']['posixpath']['ext_data'])
+        self.ext        : str       = Path(configfiles.dot_ini['paths']['ext']['external_data'])
+        
+        self.BinManager: BinManager = BinManager()
+    
+    def read(self, filename: str) -> pd.DataFrame:
+        """
+        Read the ARFF file and return it as a pandas DataFrame.
+
+        This method checks if a cached binary version of the file exists. If it
+        does, it loads the DataFrame from the binary file. Otherwise, it reads
+        the ARFF file, converts it to a DataFrame, caches it, and returns it.
+
+        Args:
+            filename (str): The name of the ARFF file (without extension) to read.
+
+        Returns:
+            pd.DataFrame: The ARFF file data as a pandas DataFrame.
+        """
+        if self.BinManager.get_bin_filenames(filename):
+            return self.BinManager.get(filename)
+
+        path_: PosixPath = Path(self.root_path, f"{filename}{self.ext}")
+        data: pd.DataFrame = pd.DataFrame(arff.loadarff(path_)[0])
+        genlog.log(True, f"read {path_}", True)
+        self.BinManager.post(filename, data)
+        return data
